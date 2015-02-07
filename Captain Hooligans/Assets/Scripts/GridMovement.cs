5aac4bf6cedc893fde7b3d988229aae579c3478c
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-02-07 14:25:00 +0200

    Player can't move through obstacles, added some trees and a ground texture

--- Captain Hooligans/Assets/Scripts/GridMovement.cs
+++ Captain Hooligans/Assets/Scripts/GridMovement.cs
@@ -3,5 +3,5 @@ using System.Collections;
 
 public class GridMovement : MonoBehaviour {
 
-    public static float tileSize = 5;	
+    public static float tileSize = 10;	
 }
--- Captain Hooligans/Assets/Scripts/PlayerFourDirMovement.cs
+++ Captain Hooligans/Assets/Scripts/PlayerFourDirMovement.cs
@@ -1,31 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Moves a gameobject depending on it's facing direction.
+/// </summary>
 public class PlayerFourDirMovement : MonoBehaviour {
 
 	public GameObject toinenPeliObjekti;
     public float movementSpeed = 0.01f;
 
-    bool moving = false;
+	MovementStatus movementStatus = MovementStatus.Still;
     Vector3 movingDirection;
     float movementProgress = 0;
     Vector3 startPosition, endPosition;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	enum MovementStatus { Still, Moving, BlockedGoingForward, BlockedGoingBackward };
 	
 	// Update is called once per frame
 	void Update () {
-//        Debug.Log("moving " + moving + " vertical " + Input.GetAxisRaw("Vertical") + " horizontal " + Input.GetAxisRaw("Horizontal"));
         bool playerGaveMoveOrder = Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0;
-        if (!moving && playerGaveMoveOrder)
+        if (movementStatus == MovementStatus.Still && playerGaveMoveOrder)
         {
             startPosition = transform.position;
-            moving = true;            
             float yRotation = transform.rotation.eulerAngles.y;
 
+			// Player's movement direction is determined by his facing direction: he moves to the direction that is 
+			// closest to his facing direction
             if (yRotation < 45 || 360 - 45 < yRotation)
                 movingDirection = Vector3.forward;
             else if (45 < yRotation && yRotation < 135)
@@ -35,6 +35,7 @@ public class PlayerFourDirMovement : MonoBehaviour {
             else if (225 < yRotation && yRotation < 360 - 45)
                 movingDirection = Vector3.left;
 
+			// If the palayer moves to some other direction than forward, rotate the movingDirection accordinlgy
             if(Input.GetAxisRaw("Horizontal") == 1)
                 movingDirection = Quaternion.Euler(new Vector3(0, 90, 0)) * movingDirection;            
             else if(Input.GetAxisRaw("Horizontal") == -1)
@@ -43,22 +44,67 @@ public class PlayerFourDirMovement : MonoBehaviour {
                 movingDirection = Quaternion.Euler(new Vector3(0, 180, 0)) * movingDirection;
         
             endPosition = startPosition + movingDirection * GridMovement.tileSize;
+
+			// Check for obstacles
+			int obstacles = LayerMask.GetMask(new string[]{"Obstacle"});
+			if (Physics.Raycast(startPosition, movingDirection, GridMovement.tileSize, obstacles))
+			{
+				movementStatus = MovementStatus.BlockedGoingForward;
+				SoundEffectManager.playSoundEffectOnce("WalkAgainstObstacle");
+			}
+			else {
+				movementStatus = MovementStatus.Moving;
+			}
         }
 
 	}
 
     void FixedUpdate() {
-        if (moving)
-        {
-            movementProgress += movementSpeed;
-            if (movementProgress > 1) {
-                movementProgress = 1;
-            }
-            transform.position = Vector3.Lerp(startPosition, endPosition, movementProgress);
-            if (movementProgress == 1) {
-                movementProgress = 0;
-                moving = false;
-            }
+		bool stop = false;
+
+		switch (movementStatus) {
+		
+			case MovementStatus.Moving:
+				movementProgress += movementSpeed;
+				if (movementProgress > 1) {
+					movementProgress = 1;
+				}
+				
+				if (movementProgress == 1) {
+					movementProgress = 1;
+					stop = true;
+					//movementStatus = MovementStatus.Still;
+	            }
+				break;
+
+			case MovementStatus.BlockedGoingForward: 
+				if(movementProgress < 0.2) {
+					movementProgress += movementSpeed;
+                }
+				else {
+					movementStatus = MovementStatus.BlockedGoingBackward;
+				}
+				break;
+
+			case MovementStatus.BlockedGoingBackward:
+				movementProgress -= movementSpeed;
+				if(movementProgress < 0) {
+					movementProgress = 0;
+					//movementStatus = MovementStatus.Still;
+					stop = true;
+                }
+				break;
+
+            default:
+                break;
         }
+
+		if (movementStatus != MovementStatus.Still) {
+			transform.position = Vector3.Lerp(startPosition, endPosition, movementProgress);
+			if(stop) {
+				movementStatus = MovementStatus.Still;
+				movementProgress = 0;
+			}
+		}        
     }
 }
