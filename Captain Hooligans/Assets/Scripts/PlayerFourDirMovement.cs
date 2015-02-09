323d4bf1c79aefbabe76257dc18ed15a8d495663
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-02-09 20:17:00 +0200

    Changed the movement script to use iTween for simplicity. Divided the long Update method to several methods.

--- Captain Hooligans/Assets/Scripts/PlayerFourDirMovement.cs
+++ Captain Hooligans/Assets/Scripts/PlayerFourDirMovement.cs
@@ -6,105 +6,127 @@ using System.Collections;
 /// </summary>
 public class PlayerFourDirMovement : MonoBehaviour {
 
-	public GameObject toinenPeliObjekti;
     public float movementSpeed = 0.01f;
 
-	MovementStatus movementStatus = MovementStatus.Still;
+	bool moving = false;
     Vector3 movingDirection;
-    float movementProgress = 0;
     Vector3 startPosition, endPosition;
 
-	enum MovementStatus { Still, Moving, BlockedGoingForward, BlockedGoingBackward };
-	
 	// Update is called once per frame
 	void Update () {
         bool playerGaveMoveOrder = Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0;
-        if (movementStatus == MovementStatus.Still && playerGaveMoveOrder)
+        if (!moving && playerGaveMoveOrder)
         {
             startPosition = transform.position;
             float yRotation = transform.rotation.eulerAngles.y;
 
-			// Player's movement direction is determined by his facing direction: he moves to the direction that is 
-			// closest to his facing direction
-            if (yRotation < 45 || 360 - 45 < yRotation)
-                movingDirection = Vector3.forward;
-            else if (45 < yRotation && yRotation < 135)
-                movingDirection = Vector3.right;
-            else if (135 < yRotation && yRotation < 225)
-                movingDirection = Vector3.back;
-            else if (225 < yRotation && yRotation < 360 - 45)
-                movingDirection = Vector3.left;
-
-			// If the palayer moves to some other direction than forward, rotate the movingDirection accordinlgy
-            if(Input.GetAxisRaw("Horizontal") == 1)
-                movingDirection = Quaternion.Euler(new Vector3(0, 90, 0)) * movingDirection;            
-            else if(Input.GetAxisRaw("Horizontal") == -1)
-                movingDirection = Quaternion.Euler(new Vector3(0, 270, 0)) * movingDirection;
-            else if(Input.GetAxisRaw("Vertical") == -1)
-                movingDirection = Quaternion.Euler(new Vector3(0, 180, 0)) * movingDirection;
-        
-            endPosition = startPosition + movingDirection * GridMovement.tileSize;
+			movingDirection = GetMovingDirectionFromOrientation (yRotation);
+			movingDirection = RotateMovingDirection (movingDirection); 
 
 			// Check for obstacles
 			int obstacles = LayerMask.GetMask(new string[]{"Obstacle"});
 			if (Physics.Raycast(startPosition, movingDirection, GridMovement.tileSize, obstacles))
 			{
-				movementStatus = MovementStatus.BlockedGoingForward;
-				SoundEffectManager.playSoundEffectOnce("WalkAgainstObstacle");
+				// If there's an obstacle in front of the player bump forward and then back a little bit.
+				endPosition = startPosition + movingDirection * GridMovement.tileSize * 0.2f;
+				BlockedMoveForward ();				
+				SoundEffectManager.playSoundEffectOnce ("WalkAgainstObstacle");
 			}
 			else {
-				movementStatus = MovementStatus.Moving;
+				// Otherwis just move to the next tile.
+				endPosition = startPosition + movingDirection * GridMovement.tileSize;
+				MoveToNextTile();
 			}
+			moving = true;
         }
 
 	}
 
-    void FixedUpdate() {
-		bool stop = false;
+	/// <summary>
+	/// Player's movement direction is determined by his facing direction: he moves to the direction that is 
+	/// closest to his facing direction.
+	/// </summary>
+	/// <returns>The moving direction.</returns>
+	/// <param name="yRotation">Y rotation.</param>
+	Vector3 GetMovingDirectionFromOrientation (float yRotation)
+	{
+		Vector3 newMovingDirection = Vector3.zero;
+		if (yRotation < 45 || 360 - 45 < yRotation)
+			newMovingDirection = Vector3.forward;
+		else
+			if (45 < yRotation && yRotation < 135)
+				newMovingDirection = Vector3.right;
+			else
+				if (135 < yRotation && yRotation < 225)
+					newMovingDirection = Vector3.back;
+				else
+					if (225 < yRotation && yRotation < 360 - 45)
+						newMovingDirection = Vector3.left;
+		return newMovingDirection;
+	}
 
-		switch (movementStatus) {
-		
-			case MovementStatus.Moving:
-				movementProgress += movementSpeed;
-				if (movementProgress > 1) {
-					movementProgress = 1;
-				}
-				
-				if (movementProgress == 1) {
-					movementProgress = 1;
-					stop = true;
-					//movementStatus = MovementStatus.Still;
-	            }
-				break;
+	/// <summary>
+	/// If the palayer moves to some other direction than forward, rotate the movingDirection accordinlgy
+	/// </summary>
+	/// <returns>The rotated moving direction.</returns>
+	/// <param name="movingDirection">The original moving direction.</param>
+	Vector3 RotateMovingDirection (Vector3 movingDirection)
+	{
+		Vector3 newMovingDirection = Vector3.zero;
+		if (Input.GetAxisRaw ("Horizontal") == 1)
+			newMovingDirection = Quaternion.Euler (new Vector3 (0, 90, 0)) * movingDirection;
+		else
+			if (Input.GetAxisRaw ("Horizontal") == -1)
+				newMovingDirection = Quaternion.Euler (new Vector3 (0, 270, 0)) * movingDirection;
+			else
+				if (Input.GetAxisRaw ("Vertical") == -1)
+					newMovingDirection = Quaternion.Euler (new Vector3 (0, 180, 0)) * movingDirection;
+				else
+					return movingDirection;
+		return newMovingDirection;
+	}
 
-			case MovementStatus.BlockedGoingForward: 
-				if(movementProgress < 0.2) {
-					movementProgress += movementSpeed;
-                }
-				else {
-					movementStatus = MovementStatus.BlockedGoingBackward;
-				}
-				break;
+	/// <summary>
+	/// Not moving anymore, so it's possible to move again.
+	/// </summary>
+	void NotMovingAnymore() {
+		moving = false;
+	}
 
-			case MovementStatus.BlockedGoingBackward:
-				movementProgress -= movementSpeed;
-				if(movementProgress < 0) {
-					movementProgress = 0;
-					//movementStatus = MovementStatus.Still;
-					stop = true;
-                }
-				break;
+	/// <summary>
+	/// Moves the player to the next tile.
+	/// </summary>
+	void MoveToNextTile() {
+		iTween.MoveTo(gameObject, iTween.Hash("x", endPosition.x,
+		                                      "y", endPosition.y,
+		                                      "z", endPosition.z,
+		                                      "speed", movementSpeed,
+		                                      "easetype", iTween.EaseType.linear,
+		                                      "oncomplete", "NotMovingAnymore"));
+	}
 
-            default:
-                break;
-        }
+	/// <summary>
+	/// The player will bump forward, then backward.
+	/// </summary>
+	void BlockedMoveForward ()
+	{
+		iTween.MoveTo (gameObject, iTween.Hash ("x", endPosition.x,
+		                                        "y", endPosition.y,
+		                                        "z", endPosition.z, 
+		                                        "speed", movementSpeed / 2,
+		                                        "easetype", iTween.EaseType.linear,
+		                                        "oncomplete", "BlockedMoveBack"));
+	}
 
-		if (movementStatus != MovementStatus.Still) {
-			transform.position = Vector3.Lerp(startPosition, endPosition, movementProgress);
-			if(stop) {
-				movementStatus = MovementStatus.Still;
-				movementProgress = 0;
-			}
-		}        
-    }
+	/// <summary>
+	/// Move backwards after moving forwards when being blocked.
+	/// </summary>
+	void BlockedMoveBack() {
+		iTween.MoveTo(gameObject, iTween.Hash("x", startPosition.x,
+		                                      "y", startPosition.y,
+		                                      "z", startPosition.z,
+		                                      "speed", movementSpeed/2,
+		                                      "easetype", iTween.EaseType.linear,
+		                                      "oncomplete", "NotMovingAnymore"));
+	}
 }
