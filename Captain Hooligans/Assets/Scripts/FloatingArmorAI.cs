ab0aa616f8370c104c88dc96627653018ea4f5de
Hemaolle <oskari.leppaaho@gmail.com>
2015-02-26 20:16:15 +0200

    ToPlayerTile added.
    Armor will follow player but will not turn.

--- Captain Hooligans/Assets/Scripts/FloatingArmorAI.cs
+++ Captain Hooligans/Assets/Scripts/FloatingArmorAI.cs
@@ -9,6 +9,7 @@ public class FloatingArmorAI : MonoBehaviour {
     bool moving = false;
     float lastMovementEnded = 0;
     FourDirMovementAndRotation movement;
+    Vector3 newMovingDirection = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -19,20 +20,61 @@ public class FloatingArmorAI : MonoBehaviour {
 	void Update () {
         if (!moving && Time.timeSinceLevelLoad - lastMovementEnded > idleBetweenActions)
         {
-            RandomizeNextAction();
+            //RandomizeNextAction();
+            transform.TransformDirection(ToPlayerTile());
+            movement.Move(ToPlayerTile(), MoveEnded);
+            moving = true;
+
         }
 	}
 
     void RandomizeNextAction()
     {
         float random = Random.Range(0f, 1f);
-//        if (random < 0.2f)
-//            movement.TurnLeft(MoveEnded);
-//        else if (random < 0.4f)
-//            movement.TurnRight(MoveEnded);
-//        else
+        if (random < 0.2f)
+            movement.TurnLeft(MoveEnded);
+        else if (random < 0.4f)
+            movement.TurnRight(MoveEnded);
+        else
             movement.Move(transform.TransformDirection(Vector3.forward), MoveEnded);
         moving = true;
+    }
+
+   
+    /// masan koodi
+
+
+  
+    Vector3 ToPlayerTile(){
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        newMovingDirection = player.transform.position - transform.position;
+        Debug.Log(player.transform.position- transform.position);
+        newMovingDirection.Normalize();
+
+        float forA, backA, leftA, rightA;
+
+        forA = Mathf.Abs(Vector3.Angle(newMovingDirection, Vector3.forward));
+        backA = Mathf.Abs(Vector3.Angle(newMovingDirection, Vector3.back));
+        leftA = Mathf.Abs(Vector3.Angle(newMovingDirection, Vector3.left));
+        rightA = Mathf.Abs(Vector3.Angle(newMovingDirection, Vector3.right));
+
+        if (forA <= 45.1)
+        {
+            return Vector3.forward;
+        } else if (backA <= 45.1)
+        {
+            return Vector3.back;
+        } else if (leftA <= 45.1)
+        {
+            return Vector3.left;
+        } else if (rightA <= 45.1)
+        {
+            return Vector3.right;
+        } else
+        {
+            return Vector3.zero;
+        }
     }
 
     void MoveEnded() {
