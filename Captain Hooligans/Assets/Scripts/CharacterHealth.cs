0170d644ed4d07d97f29028d5687b76cf15052f4
matias koskinen <masa>
2015-03-05 20:12:27 +0200

    Added CharacterHealth and DamageOnCollision

---
+++ Captain Hooligans/Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterHealth : MonoBehaviour {
+
+    public float maxHP = 10;
+    protected float currentHP;
+
+    public Alignment alignment = Alignment.Enemy;
+    
+    public enum Alignment
+    {
+        Friendly, Enemy
+    };
+
+	// Use this for initialization
+	void Start () {
+        currentHP = maxHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void TakeDamage(int damage){
+
+        currentHP = currentHP - damage;
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die(){
+        Destroy(gameObject);
+    }
+}
---
+++ Captain Hooligans/Assets/Scripts/DamageOnCollision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnCollision : MonoBehaviour {
+    public CharacterHealth.Alignment alignment = CharacterHealth.Alignment.Enemy;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCollisionEnter(Collision other){
+        GameObject collider = other.gameObject;
+        CharacterHealth targetHealth = collider.GetComponent<CharacterHealth>();
+
+        if (targetHealth != null && targetHealth.alignment != alignment)
+        {
+            targetHealth.TakeDamage(1);
+
+        }
+
+    }
+}
