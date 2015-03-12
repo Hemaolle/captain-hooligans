81734908732a35ce2aeb2bbf6a265cbdbf745efc
matias koskinen <masa>
2015-03-12 18:22:33 +0200

    SwordDeleter added

---
+++ Captain Hooligans/Assets/Scripts/SwordDeleter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordDeleter : MonoBehaviour {
+    public float deleteIn=2f;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, deleteIn);
+	}
+    /*
+    public void OnCollisionEnter(){
+        Destroy(gameObject);
+    }
+*/
+}
